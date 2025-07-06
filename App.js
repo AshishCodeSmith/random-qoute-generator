e95737a821516bcad66e4f4a1d8372ba581b7926
ASHISH SINHA <ashishsinha2026@outlook.com>
2025-07-06 23:51:10 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import QuoteGenerator from './components/QuoteGenerator';
+function App() {
+  return (
+    <div className="App">
+   <QuoteGenerator></QuoteGenerator>
+    </div>
+  );
+}
+
+export default App;
